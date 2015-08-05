4aec3808727578ef45b32a2df16ac9ccc74603b2
Vishesh Handa <me@vhanda.in>
2015-08-06 01:42:45 +0200

    Add a setup file
    This file creates the appropriate systemlinks. Setting up my dotfiles
    should be quite trivial now.

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# vim
+ln -s ${BASEDIR}/vimrc ~/.vimrc
+# ln -s ${BASEDIR}/vim/ ~/.vim
+
+# zsh
+ln -s ${BASEDIR}/zshrc ~/.zshrc
