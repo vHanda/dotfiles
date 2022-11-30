2ccfdb0553b3bd57a8e2d28d6e340582a9ac138f
Vishesh Handa <me@vhanda.in>
2022-11-30 13:29:20 +0100

    Add devlog-prev

--- home/dot_config/private_fish/functions/devlog.fish
+++ home/dot_config/private_fish/functions/devlog.fish
@@ -15,3 +15,10 @@ function devlog
 
     open $FILE
 end
+
+function devlog-prev
+    set TODAY $(date --iso-8601)
+    set FILE $(ls ~/notes/DevLog/ | sort -u | grep -v $TODAY | tail -n 1)
+
+    open $HOME/notes/DevLog/$FILE
+end
