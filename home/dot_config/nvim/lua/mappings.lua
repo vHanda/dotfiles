9ee7e94f25ecaa172300cd3d8d330a0ab0df7ff0
Vishesh Handa <me@vhanda.in>
2026-03-10 20:36:52 +0100

    neovim: Add mapping to get out of terminal mode
    The default mapping is asinine

--- home/dot_config/nvim/lua/mappings.lua
+++ home/dot_config/nvim/lua/mappings.lua
@@ -1,10 +1,7 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("t", "<ESC><ESC>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
