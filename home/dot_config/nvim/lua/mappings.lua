1b330d89f700cbc92f076a5786ac955c8ded1634
Vishesh Handa <me@vhanda.in>
2026-05-31 00:17:42 +0200

    nvim: Properly format a lua file

--- home/dot_config/nvim/lua/mappings.lua
+++ home/dot_config/nvim/lua/mappings.lua
@@ -1,7 +1,6 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("t", "<ESC><ESC>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
-
